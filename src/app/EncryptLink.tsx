07b32e4c93d67cb494f2732e20758d63916f35d6
six-two <git@six-two.dev>
2020-12-02 10:32:42 +1300

    Improved "Encrypt a link" page a bit

--- src/app/EncryptLink.tsx
+++ src/app/EncryptLink.tsx
@@ -23,25 +23,43 @@ function TextField(props: TextFieldProps) {
     </div>
 }
 
+interface OutputRowProps {
+    url: string,
+    password: string,
+}
+
+const OutputRow = (props: OutputRowProps) => {
+    let valueDom;
+    if (!props.password) {
+        valueDom = <span className="red">
+            {'ERROR: Password can not be empty'}
+        </span>
+    } else if (!props.url) {
+        valueDom = <span className="red">
+            {'ERROR: URL can not be empty'}
+        </span>
+    } else {
+        const encrypted = encrypt(props.password, props.url);
+        // dont use base64, since it requires more space
+        // use single quotes to prevent ugly escaping of quotes (which also takes space)
+        const outputString = 'crypto|' + encrypted.toString().replace(/"/g, '\'');
+        valueDom = <span>{outputString}</span>
+    }
+    return <div className="output">
+        <div className="label">Encrypted URL</div>
+        <div className="value">
+            {valueDom}
+        </div>
+    </div>
+}
+
 
 export default function EncryptLink() {
     const [password, setPassword] = useState('monkey123');
     const [url, setUrl] = useState('https://example.com');
-    const trimmedUrl = url.trim();
 
-    let useThisValue;
-    if (!password) {
-        useThisValue = 'ERROR: Password can not be empty';
-    } else if (!trimmedUrl) {
-        useThisValue = 'ERROR: URL can not be empty';
-    } else {
-        const encrypted = encrypt(password, trimmedUrl);
-        // dont use base64, since it requires more space
-        // use single quotes to prevent ugly escaping of quotes (which also takes space)
-        useThisValue = 'crypto|' + encrypted.toString().replace(/"/g, '\'');
-    }
-
-    return (
+    return <>
+        <h1>Encrypt a link</h1>
         <div className="encrypt-link">
             <TextField
                 label="Password"
@@ -51,12 +69,9 @@ export default function EncryptLink() {
                 label="URL"
                 value={url}
                 setValue={setUrl} />
-            <div className="output">
-                <div className="label">Encrypted URL</div>
-                <div className="value">
-                    {useThisValue}
-                </div>
-            </div>
+            <OutputRow
+                password={password}
+                url={url.trim()} />
         </div>
-    )
+    </>
 }
